51ba6871d2be4f5283a9ca8ec70db10c2f9d1bfb
visavadiyavrushik <visavadiyavrushik@gmail.com>
2024-06-30 13:39:41 +0530

    cursor style Update and  home page css update

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import "./globals.css";
-import AnimatedCursor from "../components/cursor/AnimatedCursor";
+import AnimatedCursor from "react-animated-cursor";
+// import AnimatedCursor from "../components/cursor/AnimatedCursor";
 // import CustomCursor from "@/components/customCursor/CustomCursor";
 
 const inter = Poppins({
@@ -21,7 +22,31 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AnimatedCursor />
+        <AnimatedCursor
+          innerSize={10}
+          outerSize={30}
+          color="0, 0, 0"
+          outerAlpha={0.2}
+          innerScale={1}
+          outerScale={2}
+          outerStyle={{
+            border: "1px solid #000",
+          }}
+          clickables={[
+            "a",
+            'input[type="text"]',
+            'input[type="email"]',
+            'input[type="number"]',
+            'input[type="submit"]',
+            'input[type="image"]',
+            "label[for]",
+            "select",
+            "textarea",
+            "button",
+            ".link",
+            "iframe",
+          ]}
+        />
         <Header />
         {/* <CustomCursor /> */}
         {children}
